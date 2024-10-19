8d2a14856e0800bcdbb63a01ae5f26235d2e4e17
Crix week 2 prac 1404 <hometv9909@gmail.com>
2024-10-19 19:04:20 +0800

    week 5

---
+++ prac_05/word_occurences.py
@@ -0,0 +1,12 @@
+word_to_count = {}
+text = input("Enter your text: ")
+words = text.split()
+
+for word in words:
+    word_to_count[word] = word_to_count.get(word, 0) + 1
+
+sorted_words = sorted(word_to_count.keys())
+max_word_length = max(len(word) for word in sorted_words)
+
+for word in sorted_words:
+    print(f"{word:{max_word_length}} : {word_to_count[word]}")
