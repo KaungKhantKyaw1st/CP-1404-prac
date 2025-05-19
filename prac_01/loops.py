402d3454d9c376240e9ef7d7c00d61aeff499cfd
KaungKhantKyaw1st <kaungkhant.kyaw@my.jcu.edu.au>
2025-05-19 13:42:08 +0800

    loop

--- prac_01/loops.py
+++ prac_01/loops.py
@@ -1,20 +1,30 @@
-# (Initial given code)
+#example
 for i in range(1, 21, 2):
     print(i, end=' ')
 print()
 
-for i in range(0, 101, 10):
+# a. count in 10 from 0 to 100
+for i in range(0, 100, 10):
     print(i, end=' ')
 print()
 
+# b. count down from 20 to 1
 for i in range(20, 0, -1):
     print(i, end=' ')
 print()
 
-n = int(input("Number of stars: "))
-for i in range(n):
-    print('*', end='')
+# c. stars problem 1
+number_of_stars = int(input("Number of stars: "))
+for i in range(number_of_stars):
+    print("*", end='')
 print()
 
-for i in range(1, n + 1):
-    print('*' * i)
+# Another solution for question c
+# number_of_stars = int(input("Number of stars: "))
+# print("*"*number_of_stars)
+
+# d. stars problem 2
+number_of_stars = int(input("Number of stars: "))
+for i in range(number_of_stars+1):
+    print("*" * i)
+print()
